75f6e6917a3f6900cf0ddbc441265c7f5a5d00cf
Денис <tequila96@bk.ru>
2023-08-08 09:59:13 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Домашка 10");
+        task1();
+    }
+    public static void task1(){
+        System.out.println("Задача 1");
+    }
+
+
+}
